a10b1968b850c9a4e0495098cc4759cd897b7b60
jun <ag16016@shibaura-it.ac.jpgit config --global user.name jungit config --global user.email ag16016@shibaura-it.ac.jpgit config --global user.name jungit config --global user.email ag16016@shibaura-it.ac.jp>
2022-07-03 22:05:18 +0900

    use styled-components

---
+++ Practice_project/src/components/UI/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Card = (props) => {
+  return <CardDiv className={props.className}>{props.children}</CardDiv>;
+};
+
+const CardDiv = styled.div`
+  background: white;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  border-radius: 10px;
+`;
+
+export default Card;
---
+++ Practice_project/src/components/Users/AddUser.js
@@ -0,0 +1,51 @@
+import React from "react";
+import styled from "styled-components";
+
+import Card from "../UI/Card";
+
+const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <StyledCard>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </StyledCard>
+  );
+};
+
+const StyledCard = styled(Card)`
+  margin: 2rem auto;
+  padding: 1rem;
+  width: 90%;
+  max-width: 40rem;
+
+  & label {
+    display: block;
+    font-weight: bold;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    font: inherit;
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    padding: 0.15rem;
+    margin-bottom: 0.5rem;
+  }
+
+  & input:focus {
+    outline: none;
+    border-color: #4f005f;
+  }
+`;
+
+export default AddUser;
