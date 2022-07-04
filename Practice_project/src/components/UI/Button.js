2b1cb1ab681e4570e5b4dd2a3c4fbde603e4a850
jun <ag16016@shibaura-it.ac.jpgit config --global user.name jungit config --global user.email ag16016@shibaura-it.ac.jpgit config --global user.name jungit config --global user.email ag16016@shibaura-it.ac.jp>
2022-07-04 15:49:43 +0900

    Make Button Component

---
+++ Practice_project/src/components/UI/Button.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = (props) => {
+  return (
+    <StyledButton className={props.className} type={props.type}>
+      {props.children}
+    </StyledButton>
+  );
+};
+
+const StyledButton = styled.button`
+  font: inherit;
+  border: 1px solid #4f005f;
+  background: #4f005f;
+  color: white;
+  padding: 0.25rem 1rem;
+  cursor: pointer;
+
+  &:hover,
+  &:active {
+    background: #741188;
+    border-color: #741188;
+
+    &:focus {
+      outline: none;
+    }
+  }
+`;
+
+export default Button;
